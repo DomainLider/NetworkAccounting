bde0b672cf8e3cc3de49ed0633395e0ac489681e
dl <rdv84@mail.ru>
2019-01-10 22:43:54 +0700

    Редактирование сети форма

--- NetworkAccounting.Web/wwwroot/ClientApp/src/forms/network/form.js
+++ NetworkAccounting.Web/wwwroot/ClientApp/src/forms/network/form.js
@@ -6,10 +6,13 @@ const FormItem = Form.Item;
 const Option = Select.Option;
 
 export default class CrudNetwork extends Component {
+  networkDisabled=true;
+  title='Редактировать сеть'
   constructor(props) {
     super(props);
     this.changePool = updateProperty.bind(this,'network.poolId');
     this.changeDescription = updatePropertyTarget.bind(this,'network.description');
+    this.changeNetwork = updatePropertyTarget.bind(this,'network.address');
     this.state = {
       pools:this.props.pools,
       network:this.props.network,
@@ -20,10 +23,10 @@ export default class CrudNetwork extends Component {
   render(){
     const {pools,network}=this.state;
     return (
-      <Modal visible title='Редактировать сеть' okText='Сохранить' onOk={()=>{this.props.onSaveNetwork(this.state.network)}} cancelText='Закрыть' onCancel={this.props.onClose}>
+      <Modal visible title={this.title} okText='Сохранить' onOk={()=>{this.props.onSaveNetwork(this.state.network)}} cancelText='Закрыть' onCancel={this.props.onClose}>
         <Form layout='vertical'>
           <FormItem label='Сеть'>
-            <Input readOnly disabled value={`${network.address}/${network.size}`}/></FormItem>
+            <Input readOnly={this.networkDisabled} disabled={this.networkDisabled} value={`${network.address}`} onChange={this.changeNetwork}/></FormItem>
           <FormItem label='Выберите тип сети' required>
             <Select onChange={this.changePool} defaultValue={this.state.network.poolId}>
               {pools.map(p=><Option key={p.id} value={p.id}>{p.description?p.description:p.name}</Option>)}
